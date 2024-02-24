1eac95390485b17c419412789a8fc1f90254b467
Omkar Garate <garateomkar143@gmail.com>
2024-02-25 03:19:29 +0530

    started product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ProductPage from './Components/ProductPage';
+import ProductPageSlider from './Components/ProductPageSlider';
 
 function App() {
   return (
     <div>
       <ProductPage/>
+      <ProductPageSlider/>
     </div>
   );
 }
