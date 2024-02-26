d98a2444c5328b2b2ee5e0d1f25076419d5c7fe3
Akshat Agrawal <agrawalakshat582@gmail.com>
2024-02-27 00:07:33 +0530

    Added Home Page

---
+++ frontend/src/Components/Home-Comp/SeasonSale.js
@@ -0,0 +1,146 @@
+import React from "react";
+import img1 from "../../Images/Home-Assets/productNow1.png";
+import img11 from "../../Images/Home-Assets/productBack.png";
+import img2 from "../../Images/Home-Assets/Cart.png";
+import img3 from "../../Images/Home-Assets/DefaultLike.png";
+import img4 from "../../Images/Home-Assets/Cart On.png";
+import img5 from "../../Images/Home-Assets/Variant2Like.png";
+
+function SeasonSale() {
+  return (
+    <>
+      <div className="sub-title">
+        <h2>Season Sale</h2>
+        <div className="product-section">
+          <div className="product-row">
+            <div className="product-item">
+              <img src={img1} alt="" className="hoverable" />
+              <div className="hover-content">
+                <img src={img2} alt="" className="hover-img" />
+                <img src={img3} alt="" className="hover-img2" />
+              </div>
+              <div className="product-details">
+                <p className="model-type">Pink Overload Over - Sized T-shirt</p>
+                <div className="price-container">
+                  <p className="price">
+                    &#8377;599 <span className="product-offer">-20% OFF</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="product-item">
+              <img src={img1} alt="" className="hoverable" />
+              <div className="hover-content">
+                <img src={img2} alt="" className="hover-img" />
+                <img src={img3} alt="" className="hover-img2" />
+              </div>
+              <div className="product-details">
+                <p className="model-type">Pink Overload Over - Sized T-shirt</p>
+                <div className="price-container">
+                  <p className="price">
+                    &#8377;599 <span className="product-offer">-20% OFF</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="product-item">
+              <img src={img1} alt="" className="hoverable" />
+              <div className="hover-content">
+                <img src={img2} alt="" className="hover-img" />
+                <img src={img3} alt="" className="hover-img2" />
+              </div>
+              <div className="product-details">
+                <p className="model-type">Pink Overload Over - Sized T-shirt</p>
+                <div className="price-container">
+                  <p className="price">
+                    &#8377;599 <span className="product-offer">-20% OFF</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="product-item">
+              <img src={img1} alt="" className="hoverable" />
+              <div className="hover-content">
+                <img src={img2} alt="" className="hover-img" />
+                <img src={img3} alt="" className="hover-img2" />
+              </div>
+              <div className="product-details">
+                <p className="model-type">Pink Overload Over - Sized T-shirt</p>
+                <div className="price-container">
+                  <p className="price">
+                    &#8377;599 <span className="product-offer">-20% OFF</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="product-row">
+            <div className="product-item">
+              <img src={img1} alt="" className="hoverable" />
+              <div className="hover-content">
+                <img src={img2} alt="" className="hover-img" />
+                <img src={img3} alt="" className="hover-img2" />
+              </div>
+              <div className="product-details">
+                <p className="model-type">Pink Overload Over - Sized T-shirt</p>
+                <div className="price-container">
+                  <p className="price">
+                    &#8377;599 <span className="product-offer">-20% OFF</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="product-item">
+              <img src={img1} alt="" className="hoverable" />
+              <div className="hover-content">
+                <img src={img2} alt="" className="hover-img" />
+                <img src={img3} alt="" className="hover-img2" />
+              </div>
+              <div className="product-details">
+                <p className="model-type">Pink Overload Over - Sized T-shirt</p>
+                <div className="price-container">
+                  <p className="price">
+                    &#8377;599 <span className="product-offer">-20% OFF</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="product-item">
+              <img src={img1} alt="" className="hoverable" />
+              <div className="hover-content">
+                <img src={img2} alt="" className="hover-img" />
+                <img src={img3} alt="" className="hover-img2" />
+              </div>
+              <div className="product-details">
+                <p className="model-type">Pink Overload Over - Sized T-shirt</p>
+                <div className="price-container">
+                  <p className="price">
+                    &#8377;599 <span className="product-offer">-20% OFF</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="product-item">
+              <img src={img1} alt="" className="hoverable" />
+              <div className="hover-content">
+                <img src={img2} alt="" className="hover-img" />
+                <img src={img3} alt="" className="hover-img2" />
+              </div>
+              <div className="product-details">
+                <p className="model-type">Pink Overload Over - Sized T-shirt</p>
+                <div className="price-container">
+                  <p className="price">
+                    &#8377;599 <span className="product-offer">-20% OFF</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SeasonSale;
---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Navbar from "./Home-Comp/Navbar";
+import TrendingNow from "./Home-Comp/TrendingNow";
+import SeasonSale from "./Home-Comp/SeasonSale";
+import Footer from "./Home-Comp/Footer";
+import RecentUserReviews from "./Home-Comp/RecentUserReviews";
+import img1 from "../Images/Latest collection.png";
+import img2 from "../Images/Men-btn.png";
+import img3 from "../Images/Women-btn.png";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      {/* Latest collection, shop men & women button */}
+      <div className="container">
+        <img src={img1} alt="" className="c1img" />
+        <div className="title-text">
+          <h1 className="c1-title">
+            <span className="smallFont">UPGRADE YOUR</span> AESTHETHIC WITH OUR
+            LATEST COLLECTION
+          </h1>
+          <button className="buy-now-button">Buy Now</button>
+        </div>
+        <div className="right-img">
+          <div className="image-container">
+            <img src={img2} alt="" />
+            <button className="btn-Shop">SHOP MEN'S</button>
+          </div>
+          <div className="image-container">
+            <img src={img3} alt="" />
+            <button className="btn-Shop">SHOP WOMEN'S</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="horizontal-line"></div>
+
+      <TrendingNow />
+      <SeasonSale />
+      <RecentUserReviews />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
